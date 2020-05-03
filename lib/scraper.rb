af13b1b5b0bf9893944a646a6a69005ed5bae046
Melissa Perez <melissaperezg.96@gmail.com>
2020-05-03 14:08:04 -0400

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,50 @@
+require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
 class Scraper
-
-  def self.scrape_index_page(index_url)
+  index_url = 'https://learn-co-curriculum.github.io/student-scraper-test-page/index.html'
     
+  def self.scrape_index_page(index_url)
+   
+    students_array = []
+    
+    doc = Nokogiri::HTML(open(index_url))
+    array = doc.css(".student-card").collect do |student|
+     hash = {
+      
+      name: student.css(".student-name").text,
+      location: student.css(".student-location").text,
+      profile_url: student.css('a')[0]['href']
+      
+     }
+    students_array << hash
+    end
+    students_array 
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    students_hash = {}
+    doc = Nokogiri::HTML(open(profile_url)) 
+    doc.css(".social-icon-container a").map do |student|
+      website = student.attribute('href').value
+      if website.include?("twitter")
+        students_hash[:twitter] = website
+      elsif website.include?("linkedin")
+        students_hash[:linkedin] = website
+        elsif website.include?("github")
+          students_hash[:github] = website
+        else
+          students_hash[:blog] = website
+        end
+      end
+        if doc.css('.profile-quote')
+          students_hash[:profile_quote] = doc.css('.profile-quote').text
+        end
+        if doc.css(".description-holder")[0].children[1]
+          students_hash[:bio] = doc.css(".description-holder")[0].children[1].text
+        end  
+      students_hash
   end
-
 end
 
--- lib/student.rb
+++ lib/student.rb
@@ -4,20 +4,20 @@ class Student
 
   @@all = []
 
-  def initialize(student_hash)
-    
+  def self.all
+    @@all
   end
 
-  def self.create_from_collection(students_array)
-    
+  def self.create_from_collection(stdt_array)
+    stdt_array.each{|student| Student.new(student)}
+  end
+
+  def initialize(stdt_hash)
+    stdt_hash.each {|key, value| self.send(("#{key}="),value)}
+    @@all << self
   end
 
   def add_student_attributes(attributes_hash)
-    
-  end
-
-  def self.all
-    
+    attributes_hash.each{|key, value| self.send(("#{key}="), value)}
   end
 end
-
